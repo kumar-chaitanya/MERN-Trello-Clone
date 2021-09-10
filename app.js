67d4a8b7d79be8ee2062057ae0156f9b93ef3ee5
Chaitanya Kumar <16103244chaitanya@gmail.com>
2021-09-10 22:34:33 +0530

    Updated Port in app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const taskRoutes = require('./routes/task')
 const isAuthenticated = require('./middleware/isAuthenticated')
 
 const app = express()
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
