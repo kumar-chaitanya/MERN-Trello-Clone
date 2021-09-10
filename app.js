be6f5d2ee4e076a9bc42e2682d9c55dca81cfa6d
Chaitanya Kumar <16103244chaitanya@gmail.com>
2021-09-10 22:20:22 +0530

    Update Folder Structure for deployment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 
 const authRoutes = require('./routes/auth')
 const projectRoutes = require('./routes/project')
@@ -21,6 +22,13 @@ app.use('/auth', authRoutes)
 app.use('/projects', isAuthenticated, projectRoutes)
 app.use('/projects/:id/boards', isAuthenticated, boardRoutes)
 app.use('/projects/:projectId/boards/:boardId/task', isAuthenticated, taskRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
   if (!err) {
